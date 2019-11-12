02264e7c58a76b028592d42657cf988021a37715
Takafumi Arakaki <aka.tkf@gmail.com>
2019-11-12 12:49:05 -0800

    Files generated by PkgTemplates

---
+++ src/InteractiveFormatter.jl
@@ -0,0 +1,5 @@
+module InteractiveFormatter
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using InteractiveFormatter
+using Test
+
+@testset "InteractiveFormatter.jl" begin
+    # Write your own tests here.
+end
